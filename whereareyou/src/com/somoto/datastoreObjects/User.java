1b678dd4e4497f9467452bcb4bba341297e4662c
adam <adam@somote.net>
2016-10-30 15:22:43 +0200

    objectify

--- whereareyou/src/com/somoto/datastoreObjects/User.java
+++ whereareyou/src/com/somoto/datastoreObjects/User.java
@@ -1,8 +1,7 @@
 package com.somoto.datastoreObjects;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
 
 @Entity
