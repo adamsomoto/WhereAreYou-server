9302ad2969b5838da61664440f46521af4642a4d
adam <adam@somote.net>
2016-10-30 14:46:30 +0200

    gson

--- whereareyou/src/com/somoto/servlets/UsersServlet.java
+++ whereareyou/src/com/somoto/servlets/UsersServlet.java
@@ -3,24 +3,29 @@ package com.somoto.servlets;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
 import com.somoto.datastoreObjects.User;
 
 public class UsersServlet extends HttpServlet {
 
+	private static final Gson GSON = new Gson();
+	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		
 		//Map<?,?> map = req.getParameterMap();
+		List<User> userList = ofy().load().type(User.class).list();
+		String json = GSON.toJson(userList);
 		//ofy().load().type(User.class).filter("umid", "")
 		resp.setContentType("text/plain");
 		resp.setStatus(HttpServletResponse.SC_OK);
-		resp.getWriter().write("hi");
+		resp.getWriter().write(json);
 	}
 	
 	
