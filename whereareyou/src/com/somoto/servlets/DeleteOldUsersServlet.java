46ee5b540f6dd1cdb2ed4ae3771a300a1cecf1e6
adam <adam@somote.net>
2016-10-31 08:39:12 +0200

    delete users

--- whereareyou/src/com/somoto/servlets/DeleteOldUsersServlet.java
+++ whereareyou/src/com/somoto/servlets/DeleteOldUsersServlet.java
@@ -3,6 +3,7 @@ package com.somoto.servlets;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
@@ -16,12 +17,12 @@ public class DeleteOldUsersServlet extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		try{	
-			long time = System.currentTimeMillis()-30*60*1000;
-			List<User> oldUsers = ofy().load().type(User.class).filter("creation_time <", time).list();
+			Date date = new Date(System.currentTimeMillis() - 30 * 60 * 1000);
+			List<User> oldUsers = ofy().load().type(User.class).filter("creation_time <", date).list();
 			ofy().delete().entities(oldUsers);
 			resp.setContentType("text/plain");
 			resp.setStatus(HttpServletResponse.SC_OK);
-			resp.getWriter().write("Deleted "+oldUsers+" users");
+			resp.getWriter().write("Deleted "+oldUsers.size()+" users");
 		}
 		catch(Exception e){
 			resp.setContentType("text/plain");
